61ed226dd4add85e4ea186b8d2d1c451114971bc
maria <mjcanache@gmail.com>
2016-04-02 12:47:21 -0430

    avances

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401010600) do
+ActiveRecord::Schema.define(version: 20160402022445) do
 
   create_table "bookings", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160401010600) do
     t.date     "ondate"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "comment",    limit: 255
   end
 
 end
